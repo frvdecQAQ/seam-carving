ba14d440ec4655ee748f6c148ec52aac9f635d70
frvdecqaq <and17@mails.tsinghua.edu.cn>
2021-01-03 04:10:22 +0800

    scale up and down

---
+++ SeamCarve.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 安頔 on 2020/12/14.
+//
+
+#include "SeamCarve.h"
---
+++ SeamCarve.h
@@ -0,0 +1,14 @@
+//
+// Created by 安頔 on 2020/12/14.
+//
+
+#ifndef SEAM_CARVING_SEAMCARVE_H
+#define SEAM_CARVING_SEAMCARVE_H
+
+
+class SeamCarve {
+
+};
+
+
+#endif //SEAM_CARVING_SEAMCARVE_H
